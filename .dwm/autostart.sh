acb865b0152efbc1d64034acf0711d0eadde5e46
compromyse <compromyse@debian.compromyse.xyz>
2022-12-11 13:37:11 +0530

    add files

---
+++ .dwm/autostart.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+/usr/lib/x86_64-linux-gnu/polkit-mate/polkit-mate-authentication-agent-1 &
+feh --bg-scale /home/compromyse/Pictures/wallpaper.jpg
+dunst &
+picom &
+/home/compromyse/.dwm/bar.sh &
+wmname LG3D
+/usr/bin/vmware-user-suid-wrapper &
---
+++ .dwm/bar.sh
@@ -0,0 +1,5 @@
+while true
+do
+	xsetroot -name "| $(date +' %d/%m/%y |  %A, %R') | λ $(whoami)"
+	sleep 5
+done
