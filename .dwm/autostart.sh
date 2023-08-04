1ffbe6d42d7b533ddeb587339a66a099811f6716
Raghuram Subramani <raghus2247@gmail.com>
2023-08-04 19:30:59 +0530

    update

--- .dwm/autostart.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-/usr/lib/x86_64-linux-gnu/polkit-mate/polkit-mate-authentication-agent-1 &
-feh --bg-scale /home/compromyse/Pictures/wallpaper.jpg
-dunst &
-picom &
-/home/compromyse/.dwm/bar.sh &
-wmname LG3D
--- .dwm/bar.sh
+++ .dwm/bar.sh
@@ -1,5 +1,39 @@
-while true
-do
-	xsetroot -name "| $(date +' %d/%m/%y |  %A, %R') | λ $(whoami)"
-	sleep 5
+#!/bin/dash
+
+interval=0
+
+black=#1e1f26
+white=#d3d7f2
+
+battery() {
+  get_capacity="$(cat /sys/class/power_supply/BAT1/capacity)"
+  printf "^b$black^ "
+  printf "^c$black^^b$white^   $get_capacity "
+  printf "^b$black^ "
+}
+
+wlan() {
+	case "$(cat /sys/class/net/wl*/operstate 2>/dev/null)" in
+	up) printf "^c$black^ ^b$white^ 󰤨 Connected ^d^";;
+	down) printf "^c$black^ ^b$white^ 󰤭 Disconnected ^d^";;
+	esac
+}
+
+clock() {
+  printf "^b$black^  "
+	printf "^c$black^^b$white^ 󱑆 $(date '+%H:%M') "
+  printf "^b$black^  "
+}
+
+user() {
+  printf "^c$black^^b$white^ $(whoami) "
+  printf "^b$black^  "
+}
+
+while true; do
+
+  [ $interval = 0 ] || [ $(($interval % 3600)) = 0 ]
+  interval=$((interval + 1))
+
+  sleep 5 && xsetroot -name "$(battery)$(wlan)$(clock)$(user)"
 done
