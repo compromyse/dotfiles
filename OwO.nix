8d1658b72bb9bf52cb462a4a3c96575baf1a79ec
Raghuram Subramani <raghus2247@gmail.com>
2024-01-27 15:42:19 +0530

    modularize config

---
+++ OwO.nix
@@ -0,0 +1,36 @@
+{ config, pkgs, ... }:
+
+{
+  boot.extraModprobeConfig = "options kvm_intel nested=1";
+
+  networking.hostName = "OwO";
+
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = false;
+    nvidiaSettings = false;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+    prime = {
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+  };
+
+  fileSystems."/data" = {
+    device = "/dev/sda";
+    fsType = "ext4";
+  };
+
+  time.timeZone = "Asia/Kolkata";
+}
