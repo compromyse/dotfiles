d6e34f60c699350203daa3e6ceaa01c15778a5a9
Raghuram Subramani <raghus2247@gmail.com>
2024-02-01 13:14:39 +0530

    .

--- config/dwl.nix
+++ config/dwl.nix
@@ -2,4 +2,30 @@
 
 {
   home.packages = [ pkgs.dwl ];
+
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "tuigreet --time --remember --cmd Hyprland";
+        user = "greeter";
+      };
+    };
+  };
+
+  services.logind.extraConfig = ''
+    HandlePowerKey=ignore
+  '';
+
+  systemd.services.greetd.serviceConfig = {
+    Type = "idle";
+    StandardInput = "tty";
+    StandardOutput = "tty";
+    StandardError = "journal";
+    TTYReset = true;
+    TTYVHangup = true;
+    TTYVTDisallocate = true;
+  };
+
+  programs.dconf.enable = true;
 }
