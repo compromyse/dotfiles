55b5ed91286cf028c0b3f09e54a3b52a0a258b8d
Raghuram Subramani <raghus2247@gmail.com>
2024-02-01 14:46:02 +0530

    temp

--- packages/dwl-custom.nix
+++ packages/dwl-custom.nix
@@ -28,7 +28,7 @@ stdenv.mkDerivation (finalAttrs: {
     owner = "compromyse";
     repo = "dwl";
     rev = "master";
-    hash = "sha256-0IQBCXJTD3+yNEGYtzamad6fMisQA62mfrjz2glnF9E=";
+    hash = "sha256-QULFeMS/4skCQeEi3XF8cZczZNvrSXi5pszrnIu3Vkc=";
   };
 
   nativeBuildInputs = [
