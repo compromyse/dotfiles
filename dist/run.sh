221970721cb649fdbd2ad0de4b7548ab25f15a3f
Raghuram Subramani <raghus2247@gmail.com>
2024-03-10 15:14:47 +0530

    replace background color

--- dist/run.sh
+++ dist/run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-bemenu-run -i -p λ -b --hp 8 --fn 'UbuntuMono Nerd Font Mono' --tb '#272727' --tf '#f2f0fa' --fb '#272727' --ff 'f2f0fa' --cb '#272727' --cf '#f2f0fa' --nb '#272727' --nf '#f2f0fa' --hb '#f2f0fa' --hf '#272727' --fbb '#272727' --fbf '#f2f0fa' --sb '#f2f0fa' --sf '#272727' --ab '#272727' --af '#f2f0fa' --scb '#272727' --scf '#272727'
+bemenu-run -i -p λ -b --hp 8 --fn 'UbuntuMono Nerd Font Mono' --tb '#131417' --tf '#f2f0fa' --fb '#131417' --ff 'f2f0fa' --cb '#131417' --cf '#f2f0fa' --nb '#131417' --nf '#f2f0fa' --hb '#f2f0fa' --hf '#131417' --fbb '#131417' --fbf '#f2f0fa' --sb '#f2f0fa' --sf '#131417' --ab '#131417' --af '#f2f0fa' --scb '#131417' --scf '#131417'
