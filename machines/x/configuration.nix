929157cb1fca28680ba281f4b43f651bd37976f5
Raghuram Subramani <raghus2247@gmail.com>
2024-03-11 10:19:45 +0530

    update

--- machines/x/configuration.nix
+++ machines/x/configuration.nix
@@ -20,15 +20,10 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  boot.extraModprobeConfig = "options kvm_intel nested=1";
+  boot.extraModprobeConfig = "options kvm_amd nested=1";
 
   nixpkgs.config.allowUnfree = true;
   nixpkgs.config.allowUnfreePredicate = _: true;
-
-  fileSystems."/data" = {
-    device = "/dev/sda";
-    fsType = "ext4";
-  };
 
   networking.hostName = "x";
 
