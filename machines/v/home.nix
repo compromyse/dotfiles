18d5951265c67bcf99237f9543f89793bd024d55
Raghuram Subramani <raghus2247@gmail.com>
2024-05-20 15:59:10 +0000

    home-manager configuration for vagrant box

--- machines/v/home.nix
+++ machines/v/home.nix
@@ -1,13 +1,11 @@
 { pkgs, ... }:
 
-let
-  tlauncher = (pkgs.callPackage ../../packages/tlauncher.nix {});
-in {
+{
   nixpkgs.config.allowUnfree = true;
 
   home = {
-    username = "compromyse";
-    homeDirectory = "/home/compromyse";
+    username = "vagrant";
+    homeDirectory = "/home/vagrant";
   };
 
   home.packages = with pkgs; [
@@ -21,35 +19,18 @@ in {
     unzip
     zip
 
-    imv
-
-    spotify
-    firefox
-    google-chrome
-
-    obs-studio
-
-    wl-clipboard
-
     ccls
     nodePackages.pyright
     git-lfs
-  ] ++ [ tlauncher ];
+  ];
 
   imports = (map (path: ../../config/${path}) [
     "bash.nix"
     "dotfiles.nix"
     "git"
     "nvim"
-
-    "dwl.nix"
-    "themes.nix"
-    "way-displays"
-    "swaylock"
-    "dunst"
-
-    "alacritty"
   ]);
 
   home.stateVersion = "23.11";
+  programs.home-manager.enable = true;
 }
