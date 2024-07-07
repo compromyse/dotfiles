bc5d1b7ac11983feec38ec25a06e2c4bcf0727fa
Raghuram Subramani <raghus2247@gmail.com>
2024-07-07 18:30:41 +0530

    update

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -9,7 +9,7 @@
     powerManagement.finegrained = true;
     open = true;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
 
     prime = {
       offload = {
