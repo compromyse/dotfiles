0e43824dfe3bdef2ad6871b9cd773834acdd1022
Raghuram Subramani <raghus2247@gmail.com>
2025-01-20 21:06:46 +0530

    update

--- modules/laptop.nix
+++ modules/laptop.nix
@@ -16,4 +16,22 @@
     kernelModules = [ "acpi_call" ];
     extraModulePackages = [ config.boot.kernelPackages.acpi_call ];
   };
+
+  environment.systemPackages = [
+    (pkgs.writeShellScriptBin "ong" ''
+      echo "\_SB.AMW3.WMAX 0 0x15 {1, 0xab, 0x00, 0x00}" > /proc/acpi/call
+      echo "\_SB.AMW3.WMAX 0 0x15 {1, 0xab, 0x00, 0x00}" > /proc/acpi/call
+      echo "\_SB.AMW3.WMAX 0 0x15 {1, 0xab, 0x00, 0x00}" > /proc/acpi/call
+      echo "\_SB.AMW3.WMAX 0 0x15 {1, 0xab, 0x00, 0x00}" > /proc/acpi/call
+      echo "\_SB.AMW3.WMAX 0 0x15 {1, 0xab, 0x00, 0x00}" > /proc/acpi/call
+    '')
+
+    (pkgs.writeShellScriptBin "offg" ''
+      echo "\_SB.AMW3.WMAX 0 0x15 {1, 0xa0, 0x00, 0x00}" > /proc/acpi/call
+      echo "\_SB.AMW3.WMAX 0 0x15 {1, 0xa0, 0x00, 0x00}" > /proc/acpi/call
+      echo "\_SB.AMW3.WMAX 0 0x15 {1, 0xa0, 0x00, 0x00}" > /proc/acpi/call
+      echo "\_SB.AMW3.WMAX 0 0x15 {1, 0xa0, 0x00, 0x00}" > /proc/acpi/call
+      echo "\_SB.AMW3.WMAX 0 0x15 {1, 0xa0, 0x00, 0x00}" > /proc/acpi/call
+    '')
+  ];
 }
