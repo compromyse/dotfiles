cfc906952301eb4c77a7a878fe6ea609c9dd438a
Raghuram Subramani <raghus2247@gmail.com>
2025-10-28 09:30:31 -0400

    remove dwlb

--- config/dwl.nix
+++ config/dwl.nix
@@ -4,7 +4,10 @@ let
   wayland-protocols = pkgs.callPackage ../packages/wayland-protocols.nix {};
   wlroots = pkgs.callPackage ../packages/wlroots.nix { inherit wayland-protocols; };
   dwl = (pkgs.callPackage ../packages/dwl.nix { inherit wlroots; inherit wayland-protocols; });
-  dwlb = (pkgs.callPackage ../packages/dwlb.nix {});
+  # dwlb = (pkgs.callPackage ../packages/dwlb.nix {});
 in {
-  home.packages = [ dwl dwlb ];
+  home.packages = [
+    dwl
+    # dwlb
+  ];
 }
