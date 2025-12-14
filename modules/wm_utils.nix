b8d400a0ad4b04f18121e35268ad64ea4a62c40a
Raghuram Subramani <raghus2247@gmail.com>
2025-12-13 21:27:05 -0500

    flakes: update

--- modules/wm_utils.nix
+++ modules/wm_utils.nix
@@ -23,9 +23,18 @@
     bemenu
   ];
 
+  # xdg.portal = {
+  #     enable = true;
+  #     extraPortals = [ pkgs.xdg-desktop-portal-wlr pkgs.xdg-desktop-portal ];
+  #     config.common.default = "wlr";
+  # };
+
   xdg.portal.enable = true;
-  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-wlr pkgs.xdg-desktop-portal ];
   xdg.portal.config.common.default = "wlr";
+  xdg.portal.wlr = {
+    enable = true;
+    settings.screencast.chooser_cmd = "${pkgs.slurp}/bin/slurp -f %o -o";
+  };
 
   environment.variables = {
     XCURSOR_SIZE = "16";
