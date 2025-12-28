f3d8dcaff71aedbd6ed0924081105054bdcf3504
Raghuram Subramani <raghus2247@gmail.com>
2025-12-28 15:48:27 -0800

    headscale: get service running

--- modules/headscale.nix
+++ modules/headscale.nix
@@ -1,29 +1,51 @@
-{ pkgs, config, lib, ... }:
+{ config, ... }:
 
 let
-  basedomain = "compromyse.xyz";
-  subdomain = "headscale";
+  derpPort = 3478;
 in {
   services = {
     headscale = {
       enable = true;
-      address = "0.0.0.0";
-      port = 8080;
-      server_url = "https://${subdomain}.${basedomain}";
-      dns = { baseDomain = basedomain; };
-      settings = { logtail.enabled = false; };
+      port = 8085;
+      address = "127.0.0.1";
+      settings = {
+        dns = {
+          override_local_dns = true;
+          base_domain = "me";
+          magic_dns = true;
+          nameservers.global = ["8.8.8.8"];
+        };
+        server_url = "https://headscale.compromyse.xyz";
+        metrics_listen_addr = "127.0.0.1:8095";
+        logtail.enabled = false;
+        log.level = "warn";
+        derp.server = {
+          enable = true;
+          region_id = 999;
+          stun_listen_addr = "0.0.0.0:${toString derpPort}";
+        };
+      };
     };
 
-    nginx.virtualHosts."${subdomain}.${basedomain}" = {
-      forceSSL = true;
-      enableACME = true;
-      locations."/" = {
-        proxyPass =
-          "http://localhost:${toString config.services.headscale.port}";
-        proxyWebsockets = true;
+    nginx.virtualHosts = {
+      "headscale.compromyse.xyz" = {
+        forceSSL = true;
+        enableACME = true;
+        locations = {
+          "/" = {
+            proxyPass = "http://localhost:${toString config.services.headscale.port}";
+            proxyWebsockets = true;
+          };
+          "/metrics" = {
+            proxyPass = "http://${config.services.headscale.settings.metrics_listen_addr}/metrics";
+          };
+        };
       };
     };
   };
 
-  environment.systemPackages = [ config.services.headscale.package ];
+  # Derp server
+  networking.firewall.allowedUDPPorts = [derpPort];
+
+  environment.systemPackages = [config.services.headscale.package];
 }
